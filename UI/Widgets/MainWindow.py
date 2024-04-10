50aff2bebb8305f4ce74768811609590727105c4
Ugine <makutin_ea@mail.ru>
2024-04-10 23:30:45 +0300

    Изменил стиль MenuBar согласно дизайн-макету

--- UI/Widgets/MainWindow.py
+++ UI/Widgets/MainWindow.py
@@ -1,4 +1,47 @@
-from PySide6.QtWidgets import (QMainWindow, QSizePolicy)
+from PySide6.QtWidgets import (QMainWindow, QSizePolicy, QHBoxLayout,
+                               QToolBar, QLabel, QPushButton,
+                               QSpacerItem, QWidget)
+from PySide6.QtGui import QFont, QIcon
+from PySide6.QtCore import QSize, Qt
+
+class UserPanel(QWidget):
+    def __init__(self):
+        super().__init__()
+        # =====================================================================#
+        #                               Фронт                                  #
+        # =====================================================================#
+        # Элементы виджета
+        # Название приложения
+        app_title_label = QLabel()
+        app_title_label.setText("ShipTransOpt")
+        # Иконка пользователя
+        user_icon = QIcon("UI/Icons/Person _Icon.svg")
+        user_photo = QLabel()
+        user_photo.setPixmap(user_icon.pixmap(QSize(30, 30)))
+        # Никнейм пользователя
+        user_name = QLabel("Ugine")
+        # Бургер меню профиля
+        user_menu_button = QPushButton()
+        user_menu_button.setIcon(QIcon("UI/Icons/Burger_menu_Icon.svg"))
+        # Spacers
+        horizontalSpacer_1 = QSpacerItem(510, 20, QSizePolicy.Maximum, QSizePolicy.Minimum)
+        horizontalSpacer_2 = QSpacerItem(710, 20, QSizePolicy.Maximum, QSizePolicy.Minimum)
+        horizontalSpacer_3 = QSpacerItem(30, 20, QSizePolicy.Maximum, QSizePolicy.Minimum)
+
+        # Слой
+        horizontalLayout = QHBoxLayout()
+        # Добавляю элементы на горизонтальный слой
+        horizontalLayout.addItem(horizontalSpacer_1)
+        horizontalLayout.addWidget(app_title_label)
+        horizontalLayout.addItem(horizontalSpacer_2)
+        horizontalLayout.addWidget(user_photo)
+        horizontalLayout.addItem(horizontalSpacer_3)
+        horizontalLayout.addWidget(user_name)
+        horizontalLayout.addItem(horizontalSpacer_3)
+        horizontalLayout.addWidget(user_menu_button)
+
+        # Отображаем слой на виджете
+        self.setLayout(horizontalLayout)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -8,17 +51,26 @@ class MainWindow(QMainWindow):
         # =====================================================================#
         # Название приложения
         self.setWindowTitle("ShipTransOpt")
-
+        
         # Menubar
-        menu_bar = self.menuBar()
-        file_menu = menu_bar.addMenu("Файл")
-        open_action = file_menu.addAction("Открыть")
-        quit_action = file_menu.addAction("Закрыть")
+        # Элементы верхнего меню
+        # Основные кнопки на верхнем меню
+        menu_bar = QToolBar()
+        menu_bar.setMovable(False)
+        self.addToolBar(menu_bar)
+        file_menu = menu_bar.addAction("Файл")
         save_action = menu_bar.addAction("Сохранить")
         load_action = menu_bar.addAction("Загрузить")
-        help_menu = menu_bar.addMenu("Помощь")
+        help_menu = menu_bar.addAction("Помощь")
 
-
+        # Виджет с данными пользователя
+        user_widget = UserPanel()
+        # Добавляем элементы в MenuBar
+        menu_bar.addWidget(user_widget)
+        
+        # Шрифт
+        font = QFont("Arial", 14)
+        self.setFont(font)
         # =====================================================================#
         #                               Коннекты                               #
         # =====================================================================#
@@ -39,6 +91,7 @@ class MainWindow(QMainWindow):
         self.setStyleSheet(
             """
                 background-color: rgb(184, 212, 255);
+                color: white;
             """
         )
         menu_bar.setStyleSheet(
