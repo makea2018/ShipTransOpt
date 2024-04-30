4a3282ffdfae2d539e10bc3283393dfb465d5443
Ugine <makutin_ea@mail.ru>
2024-04-30 19:15:32 +0300

    Добавил видимость атрибутам для взаимодействия извне

--- UI/Widgets/LeftPanelWidget.py
+++ UI/Widgets/LeftPanelWidget.py
@@ -10,86 +10,89 @@ class Left_Panel(QWidget):
         # =====================================================================#
         #                               Фронт                                  #
         # =====================================================================#
+        # Исходные значения предикторов
+        self.predictors_values = {}
+        
         # Параметры модели
         # Название судна
-        vessel_title = QLineEdit()
-        vessel_title.setPlaceholderText("Название судна")
+        self.vessel_title = QLineEdit()
+        self.vessel_title.setPlaceholderText("Название судна")
         # Тип судна
-        vessel_type = QComboBox()
-        vessel_type.setPlaceholderText("Тип судна")
-        vessel_type.addItem("сухогруз")
-        vessel_type.addItem("танкер")
+        self.vessel_type = QComboBox()
+        self.vessel_type.setPlaceholderText("Тип судна")
+        self.vessel_type.addItem("сухогруз")
+        self.vessel_type.addItem("танкер")
         # Длина судна
-        L = QLineEdit()
-        L.setPlaceholderText("Длина судна, м")
+        self.L = QLineEdit()
+        self.L.setPlaceholderText("Длина судна, м")
         # Ширина судна
-        B = QLineEdit()
-        B.setPlaceholderText("Ширина судна, м")
+        self.B = QLineEdit()
+        self.B.setPlaceholderText("Ширина судна, м")
         # Осадка судна
-        d = QLineEdit()
-        d.setPlaceholderText("Осадка судна, м")
+        self.d = QLineEdit()
+        self.d.setPlaceholderText("Осадка судна, м")
         # Дедвейт
-        DW = QLineEdit()
-        DW.setPlaceholderText("Дедвейт, т")
+        self.DW = QLineEdit()
+        self.DW.setPlaceholderText("Дедвейт, т")
         # Скорость судна
-        speed = QLineEdit()
-        speed.setPlaceholderText("Скорость судна, узел")
+        self.speed = QLineEdit()
+        self.speed.setPlaceholderText("Скорость судна, узел")
         # Количество перевозимого груза
-        cargo_amount = QLineEdit()
-        cargo_amount.setPlaceholderText("Количество перевозимого груза, т")
+        self.cargo_amount = QLineEdit()
+        self.cargo_amount.setPlaceholderText("Количество перевозимого груза, т")
         # Цена 1т груза за 1 милю
-        cost_per_mile = QLineEdit()
-        cost_per_mile.setPlaceholderText("Цена 1т груза за 1 милю")
+        self.cost_per_mile = QLineEdit()
+        self.cost_per_mile.setPlaceholderText("Цена 1т груза за 1 милю")
         # Спрос на товар
-        cargo_demand = QComboBox()
-        cargo_demand.setPlaceholderText("Спрос на товар")
-        cargo_demand.addItem("маленький")
-        cargo_demand.addItem("большой")
+        self.cargo_demand = QComboBox()
+        self.cargo_demand.setPlaceholderText("Спрос на товар")
+        self.cargo_demand.addItem("маленький")
+        self.cargo_demand.addItem("большой")
         # Ценность груза
-        cargo_value = QComboBox()
-        cargo_value.setPlaceholderText("Ценность груза")
-        cargo_value.addItem("обычный")
-        cargo_value.addItem("ценный")
-        cargo_value.addItem("очень ценный")
+        self.cargo_value = QComboBox()
+        self.cargo_value.setPlaceholderText("Ценность груза")
+        self.cargo_value.addItem("обычный")
+        self.cargo_value.addItem("ценный")
+        self.cargo_value.addItem("очень ценный")
         # Хрупкость груза
-        cargo_fragility = QComboBox()
-        cargo_fragility.setPlaceholderText("Хрупкость груза")
-        cargo_fragility.addItem("не хрупкий")
-        cargo_fragility.addItem("хрупкий")
+        self.cargo_fragility = QComboBox()
+        self.cargo_fragility.setPlaceholderText("Хрупкость груза")
+        self.cargo_fragility.addItem("не хрупкий")
+        self.cargo_fragility.addItem("хрупкий")
         # Класс опасности груза
-        cargo_danger = QComboBox()
-        cargo_danger.setPlaceholderText("Класс опасности груза")
-        cargo_danger.addItem("не опасный")
-        cargo_danger.addItem("опасный")
+        self.cargo_danger = QComboBox()
+        self.cargo_danger.setPlaceholderText("Класс опасности груза")
+        self.cargo_danger.addItem("не опасный")
+        self.cargo_danger.addItem("опасный")
         # Протяженность морского пути
-        sea_route = QLineEdit()
-        sea_route.setPlaceholderText("Протяженность морского пути, миль")
+        self.sea_route = QLineEdit()
+        self.sea_route.setPlaceholderText("Протяженность морского пути, миль")
         # Сила ветра
-        wind_strength = QLineEdit()
-        wind_strength.setPlaceholderText("Сила ветра, балл")
+        self.wind_strength = QLineEdit()
+        self.wind_strength.setPlaceholderText("Сила ветра, балл")
         # Сила волнения
-        sea_state = QLineEdit()
-        sea_state.setPlaceholderText("Сила волнения, балл")
+        self.sea_state = QLineEdit()
+        self.sea_state.setPlaceholderText("Сила волнения, балл")
 
         # Слои
         verticalLayout = QVBoxLayout()
         # Добавляем элементы параметров в вертикальный слой
-        verticalLayout.addWidget(vessel_title)
-        verticalLayout.addWidget(vessel_type)
-        verticalLayout.addWidget(L)
-        verticalLayout.addWidget(B)
-        verticalLayout.addWidget(d)
-        verticalLayout.addWidget(DW)
-        verticalLayout.addWidget(speed)
-        verticalLayout.addWidget(cargo_amount)
-        verticalLayout.addWidget(cost_per_mile)
-        verticalLayout.addWidget(cargo_demand)
-        verticalLayout.addWidget(cargo_value)
-        verticalLayout.addWidget(cargo_fragility)
-        verticalLayout.addWidget(cargo_danger)
-        verticalLayout.addWidget(sea_route)
-        verticalLayout.addWidget(wind_strength)
-        verticalLayout.addWidget(sea_state)
+        verticalLayout.addWidget(self.vessel_title)
+        verticalLayout.addWidget(self.vessel_type)
+        verticalLayout.addWidget(self.L)
+        verticalLayout.addWidget(self.B)
+        verticalLayout.addWidget(self.d)
+        verticalLayout.addWidget(self.DW)
+        verticalLayout.addWidget(self.speed)
+        verticalLayout.addWidget(self.cargo_amount)
+        verticalLayout.addWidget(self.cost_per_mile)
+        verticalLayout.addWidget(self.cargo_demand)
+        verticalLayout.addWidget(self.cargo_value)
+        verticalLayout.addWidget(self.cargo_fragility)
+        verticalLayout.addWidget(self.cargo_danger)
+        verticalLayout.addWidget(self.sea_route)
+        verticalLayout.addWidget(self.wind_strength)
+        verticalLayout.addWidget(self.sea_state)
 
         # Отображаем слой на виджете
         self.setLayout(verticalLayout)
@@ -97,22 +100,22 @@ class Left_Panel(QWidget):
         # Шрифт
         font = QFont("Arial", 16)
         # Применяем шрифт к объектам
-        vessel_title.setFont(font)
-        vessel_type.setFont(font)
-        L.setFont(font)
-        B.setFont(font)
-        d.setFont(font)
-        DW.setFont(font)
-        speed.setFont(font)
-        cargo_amount.setFont(font)
-        cost_per_mile.setFont(font)
-        cargo_demand.setFont(font)
-        cargo_value.setFont(font)
-        cargo_fragility.setFont(font)
-        cargo_danger.setFont(font)
-        sea_route.setFont(font)
-        wind_strength.setFont(font)
-        sea_state.setFont(font)
+        self.vessel_title.setFont(font)
+        self.vessel_type.setFont(font)
+        self.L.setFont(font)
+        self.B.setFont(font)
+        self.d.setFont(font)
+        self.DW.setFont(font)
+        self.speed.setFont(font)
+        self.cargo_amount.setFont(font)
+        self.cost_per_mile.setFont(font)
+        self.cargo_demand.setFont(font)
+        self.cargo_value.setFont(font)
+        self.cargo_fragility.setFont(font)
+        self.cargo_danger.setFont(font)
+        self.sea_route.setFont(font)
+        self.wind_strength.setFont(font)
+        self.sea_state.setFont(font)
 
 
         # =====================================================================#
@@ -126,22 +129,22 @@ class Left_Panel(QWidget):
         # Размеры виджета
         self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         self.setMaximumWidth(400)
-        vessel_title.setMaximumSize(QSize(400, 40))
-        vessel_type.setMaximumSize(QSize(400, 40))
-        L.setMaximumSize(QSize(400, 40))
-        B.setMaximumSize(QSize(400, 40))
-        d.setMaximumSize(QSize(400, 40))
-        DW.setMaximumSize(QSize(400, 40))
-        speed.setMaximumSize(QSize(400, 40))
-        cargo_amount.setMaximumSize(QSize(400, 40))
-        cost_per_mile.setMaximumSize(QSize(400, 40))
-        cargo_demand.setMaximumSize(QSize(400, 40))
-        cargo_value.setMaximumSize(QSize(400, 40))
-        cargo_fragility.setMaximumSize(QSize(400, 40))
-        cargo_danger.setMaximumSize(QSize(400, 40))
-        sea_route.setMaximumSize(QSize(400, 40))
-        wind_strength.setMaximumSize(QSize(400, 40))
-        sea_state.setMaximumSize(QSize(400, 40))
+        self.vessel_title.setMaximumSize(QSize(400, 40))
+        self.vessel_type.setMaximumSize(QSize(400, 40))
+        self.L.setMaximumSize(QSize(400, 40))
+        self.B.setMaximumSize(QSize(400, 40))
+        self.d.setMaximumSize(QSize(400, 40))
+        self.DW.setMaximumSize(QSize(400, 40))
+        self.speed.setMaximumSize(QSize(400, 40))
+        self.cargo_amount.setMaximumSize(QSize(400, 40))
+        self.cost_per_mile.setMaximumSize(QSize(400, 40))
+        self.cargo_demand.setMaximumSize(QSize(400, 40))
+        self.cargo_value.setMaximumSize(QSize(400, 40))
+        self.cargo_fragility.setMaximumSize(QSize(400, 40))
+        self.cargo_danger.setMaximumSize(QSize(400, 40))
+        self.sea_route.setMaximumSize(QSize(400, 40))
+        self.wind_strength.setMaximumSize(QSize(400, 40))
+        self.sea_state.setMaximumSize(QSize(400, 40))
 
 
         # =====================================================================#
@@ -181,3 +184,5 @@ class Left_Panel(QWidget):
     # =====================================================================#
     #                               Функции                                #
     # =====================================================================#
+    def add_value_to_predictors_dict(self):
+        pass
