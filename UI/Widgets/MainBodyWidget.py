80165136b1a07dadaa8de5a0465ffef7bf403be4
Ugine <makutin_ea@mail.ru>
2024-05-01 15:08:39 +0300

    Добавлено преобразование предикторов из str в int

--- UI/Widgets/MainBodyWidget.py
+++ UI/Widgets/MainBodyWidget.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import (QSpacerItem, QHBoxLayout,
                                QWidget, QSizePolicy, QLabel)
 from Widgets.LeftPanelWidget import Left_Panel
 from Widgets.RightPanelWidget import Right_Panel
+import numpy as np
 
 class NotValidLabel(QWidget):
     def __init__(self, text):
@@ -72,7 +73,6 @@ class MainBody(QWidget):
         # =====================================================================#
         #                               Коннекты                               #
         # =====================================================================#
-        self.rightPanelWidget.predict_button.clicked.connect(self.validate_na_in_predictors_values)
         self.rightPanelWidget.predict_button.clicked.connect(self.predict_cost)
 
         # =====================================================================#
@@ -90,15 +90,6 @@ class MainBody(QWidget):
     # =====================================================================#
     #                               Функции                                #
     # =====================================================================#
-    def predict_cost(self):
-        init_text = self.rightPanelWidget.result_text_label.text()
-        if init_text.endswith("- "):
-            # Вычисление стоимости транспортировки груза
-            cost = 1000
-            # Запись стоимости в QLabel
-            self.rightPanelWidget.result_text_label.setText(init_text + str(cost))
-        print(self.predictors_values)
-
     def write_predictors_values(self):
         # Записываем все значения предикторов в словарь предикторов
         self.predictors_values["vessel_title"] = self.leftPanelWidget.vessel_title.text()
@@ -128,6 +119,43 @@ class MainBody(QWidget):
             # Добавляем виджет о том, что данные не валидированы
             Warning_Label = NotValidLabel("Не все значения предикторов были заполнены!")
             self.rightPanelWidget.verticalLayout.addWidget(Warning_Label)
+            return False
         elif all(x != "" for x in list(predictors_values.values())) and not cond:
             widget = self.rightPanelWidget.verticalLayout.takeAt(3)
             self.rightPanelWidget.verticalLayout.removeWidget(widget.widget().deleteLater())
+            return True
+        
+        return True
+
+    def predict_cost(self):
+        validation_result = self.validate_na_in_predictors_values()
+        if validation_result:
+            # Обработка результатов числовых предикторов
+            self.predictors_values["L"] = int(self.predictors_values["L"])
+            self.predictors_values["B"] = int(self.predictors_values["B"])
+            self.predictors_values["d"] = int(self.predictors_values["d"])
+            self.predictors_values["DW"] = int(self.predictors_values["DW"])
+            self.predictors_values["speed"] = int(self.predictors_values["speed"])
+            self.predictors_values["cargo_amount"] = int(self.predictors_values["cargo_amount"])
+            self.predictors_values["cost_per_mile"] = int(self.predictors_values["cost_per_mile"])
+            self.predictors_values["sea_route"] = int(self.predictors_values["sea_route"])
+            self.predictors_values["wind_strength"] = int(self.predictors_values["wind_strength"])
+            self.predictors_values["sea_state"] = int(self.predictors_values["sea_state"])
+
+            # Обработка результатов категориальных предикторов
+            self.predictors_values["vessel_type"]
+            self.predictors_values["cargo_demand"]
+            self.predictors_values["cargo_value"]
+            self.predictors_values["cargo_fragility"]
+            self.predictors_values["cargo_danger"]
+    
+            print(self.predictors_values)
+        else:
+            print("Данные нельзя отправлять в нейросеть!")
+        # init_text = self.rightPanelWidget.result_text_label.text()
+        # if init_text.endswith("- "):
+        #     # Вычисление стоимости транспортировки груза
+        #     cost = 1000
+        #     # Запись стоимости в QLabel
+        #     self.rightPanelWidget.result_text_label.setText(init_text + str(cost))
+        # print(self.predictors_values)
